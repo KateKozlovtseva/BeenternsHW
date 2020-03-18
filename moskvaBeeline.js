f7c5ccf3fd8196935ecde842cf61fc07a729f8c9
Kate <ko-cat@yandex.ru>
2020-03-18 13:57:09 +0300

    Добавление ожиданий, оптимизация кода

--- moskvaBeeline.js
+++ moskvaBeeline.js
@@ -20,59 +20,51 @@ describe("checkout Google", () => {
 
     it("Открытие страницы магазина", async()=>{
         await driver.get("https://moskva.beeline.ru/shop/");
-        await driver.sleep(4000);
         let title = await driver.getTitle();
         assert(title, "Интернет-магазин Билайн Москва - продажа сотовых телефонов, смартфонов и аксессуаров");    
         });
 
     it("Перейти на страницу телефоны", async()=>{
-        let tel = await driver.findElement(By.xpath("//a[contains(@class,'DevicesMenu_component') and contains(@href,'telefony')]"));
-        await tel.click();
+        await driver.findElement(By.xpath("//a[contains(@class,'DevicesMenu_component') and contains(@href,'telefony')]")).click();
         let title = await driver.getTitle();
         assert.equal(title, "Смартфоны — купить смартфон, цены на телефоны в интернет-магазине Билайн Москва");
     });
 
     it("Показать всех производителей", async()=>{
-        let maker = await driver.findElement(By.xpath("//div[contains(@class,'FiltersWidget_container')] / div[.//span[text()='Производитель']]"));
-        let showAll = await maker.findElement(By.xpath("div[./span[text()='Показать все']]"));
-        await showAll.click();
-        await driver.sleep(1000);
+        await driver.wait(until.elementLocated(By.xpath("//div[./span[text()='Производитель']]/following-sibling::*[contains(@class,'ShowAllButton')]")), 10000).click();
+        //let maker = await driver.findElement(By.xpath("//div[contains(@class,'FiltersWidget_container')] / div[.//span[text()='Производитель']]"));
+        //await maker.findElement(By.xpath("div[./span[text()='Показать все']]")).click();
     });  
     
     it("Показать телефоны Nokia", async()=>{  //Meizu среди производителей отсутствует
-        let nokiaCheckbox = await driver.findElement(By.xpath("//input[@type='checkbox' and contains(@id,'nokia')]"));
-        await nokiaCheckbox.click();
-        await driver.sleep(5000);
-        let title = await driver.findElement(By.xpath("//h1")).getText();
-        assert.equal(title, "Смартфоны производитель Nokia");
+        await driver.wait(until.elementLocated(By.xpath("//input[@type='checkbox' and contains(@id,'nokia')]")),10000).click();
+        const title = await driver.findElement(By.xpath("//h1"));
+        await driver.wait(until.elementTextIs(title, 'Смартфоны производитель Nokia'), 10000);
     }); 
     
     it("Отсортировать телефоны по цене", async()=>{ 
-        let costSort = await driver.findElement(By.xpath("//div[contains(@class,'FilterTabs_clickable')]"));
-        costSort.click();
-        await driver.sleep(3000);
-        let costSortactive = await driver.findElement(By.xpath("//div[contains(@class,'FilterTabs_active') and contains(@class,'FilterTabs_clickable')]")).getText();
+        await driver.findElement(By.xpath("//div[contains(@class,'FilterTabs_clickable')]")).click();
+        const costSortactive = await driver.wait(until.elementLocated(By.xpath("//div[contains(@class,'FilterTabs_active') and contains(@class,'FilterTabs_clickable')]")),10000).getText();
         assert.equal(costSortactive,"Цене");
     });
     
    it("Купить телефон из списка", async()=>{ 
-        let buttonBuy = await driver.findElement(By.xpath("(//button[contains(@class,'BuyButtonLayout_button')])[7]"));
-        buttonBuy.click();
-        await driver.sleep(3000);
-        let goBack = await driver.findElement(By.xpath("//a[@onclick='QA.Beeline.PageHistory.GoPrevious();']")).getText();
+        await driver.sleep(1000);
+        const buttonBuy = await driver.wait(until.elementLocated(By.xpath("(//button[contains(@class,'BuyButtonLayout_button')])[7]")),10000);
+        await driver.wait(until.elementIsEnabled(buttonBuy)).click();
+        const goBack = await driver.wait(until.elementLocated(By.xpath("//a[@onclick='QA.Beeline.PageHistory.GoPrevious();']")),10000).getText();
         assert.equal(goBack, "Вернуться в магазин");
-        await driver.sleep(3000);
     });
 
   it("Удалить телефон из корзины", async()=>{ 
-        let cross = await driver.findElement(By.xpath("//*[local-name()='svg' and contains(@class,'modify-link-after')]"));
-        cross.click();
-        await driver.sleep(3000);
+        await driver.wait(until.elementLocated(By.xpath("//*[local-name()='svg' and contains(@class,'modify-link-after')]")),10000).click();
+        const restore = await driver.wait(until.elementLocated(By.xpath("//span[@data-ng-click='restoreItem(item)']")),10000).getText();
+        assert.equal(restore, "Восстановить");
     });
 
    it("Восстановить удаленный элемент", async()=>{ 
-        let away = await driver.findElement(By.xpath("//span[@data-ng-click='restoreItem(item)']"));
-        away.click();
+        await driver.findElement(By.xpath("//span[@data-ng-click='restoreItem(item)']")).click();
+        await driver.wait(until.elementLocated(By.xpath("//*[local-name()='svg' and contains(@class,'modify-link-after')]")),10000);
     });
 
     after(() => {
